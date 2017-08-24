de32045b2148934b56cf7c93010a4395f5f80db7
Peng Wang <peng.wang@uptake.com>
2017-08-23 19:23:37 -0500

    use npm run all

--- kill_dev_server.sh
+++
@@ -1,2 +0,0 @@
-cat testem_webpack_dev_server.pid | xargs kill -9 
-rm testem_webpack_dev_server.pid
--- start_dev_server.sh
+++
@@ -1,2 +0,0 @@
-webpack-dev-server --inline --config webpack.config.js --port 8080 &
-echo $! > testem_webpack_dev_server.pid
